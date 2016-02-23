2aba04534b1ef8e892298e85ea8f1c8a21992f0f
AnissC <aniss.cherkaoui@gmail.com>
2016-02-23 10:38:17 +0100

    Code of the communication between server and iOS for signUp and LogIn /!\ not yet tested  /!\  because i need server side

--- TraceYourFriends-IOS/LogInViewController.swift
+++ TraceYourFriends-IOS/LogInViewController.swift
@@ -32,24 +32,87 @@ class LogInViewController: UIViewController {
         let userEmail:String? = userEmailTextField.text
         let userPassword:String? = userPasswordTextField.text
         
-        let userEmailStored = NSUserDefaults.standardUserDefaults().stringForKey("userEmail")
+        if((userEmail!.isEmpty) || (userPassword!.isEmpty)){
+            displayErrorMessage("User email or password are empty !")
+        }
         
-        let userPasswordStored = NSUserDefaults.standardUserDefaults().stringForKey("userPassword")
+
+                            //======== Send user data to server side ========//
         
-        var userPassDontMatch = false
+        //create the url with NSURL
+        let myUrl = NSURL(string: "http://localhost:8080/LogIn")
         
-        if (userEmailStored == userEmail){
-            if(userPasswordStored == userPassword){
-                NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLogIn")
-                NSUserDefaults.standardUserDefaults().synchronize()
-                self.dismissViewControllerAnimated(true, completion: nil)
-                userPassDontMatch = true
+        //now create the NSMutableRequest object using the url object
+        let request = NSMutableURLRequest(URL:myUrl!)
+        
+        //create the session object
+        let session = NSURLSession.sharedSession()
+        
+        //declare parameter as a dictionary which contains string as key and value combination.
+        let jsonString : NSString =  "email=\(userEmail)&password=\(userPassword)"
+        
+        //set http method as POST
+        request.HTTPMethod = "POST"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        // pass dictionary to nsdata object and set it as request body
+        do {
+            let param = jsonString.dataUsingEncoding(NSUTF8StringEncoding)
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(param!, options: [])
+        } catch {
+            print(error)
+            request.HTTPBody = nil
+        }
+        
+        
+                            //======== Create task & Execute it ========//
+        
+        
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            guard error == nil
+                else
+            {
+                return
             }
-        }
+            print("Response: \(response)")
+            let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("Body: \(strData)")
+            let json: NSDictionary?
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? NSDictionary
+            } catch let dataError {
+                print(dataError)
+                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print("Error could not parse JSON: '\(jsonStr)'")
+                return
+            }
+            
+            // The JSONObjectWithData constructor didn't return an error. But, we should still
+            // check and make sure that json has a value using optional binding.
+            if let parseJSON = json {
+                // Okay, the parsedJSON is here, let's get the value for 'status' out of it
+                let resultValue = parseJSON["status"] as? String
+                print("result: \(resultValue)")
+                
+                if (resultValue == "Succes"){
+                    
+                    //Then LogIn is successfully done
+                    NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLogIn")
+                    NSUserDefaults.standardUserDefaults().synchronize()
+                    
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                }
+            }
+            
+        })
         
-        if (!userPassDontMatch){
-            displayErrorMessage("Email or Password don't match")
-        }
+        task.resume()
         
     }
     func displayErrorMessage(userMessage:String){
